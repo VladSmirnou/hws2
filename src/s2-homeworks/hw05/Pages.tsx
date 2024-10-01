35dbba2ac7c89dd9c4fa598c52d3485803b44f28
VladSmirnou <smr.vladislav@gmail.com>
2024-10-01 11:32:19 +0300

    fix input responsiveness issue && user message boxs triangle visibility problem

--- src/s2-homeworks/hw05/Pages.tsx
+++ src/s2-homeworks/hw05/Pages.tsx
@@ -21,7 +21,7 @@ function Pages() {
                 {/*<Route ...*/}
                 <Route path='/' element={<Navigate to={PATH.PRE_JUNIOR} />} />
                 {/*роуты для /pre-junior, /junior, /junior-plus*/}
-                
+
                 <Route path={PATH.PRE_JUNIOR}  element={<PreJunior />} />
                 <Route path={PATH.JUNIOR} element={<Junior />} />
                 <Route path={PATH.JUNIOR_PLUS} element={<JuniorPlus />} />
